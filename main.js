65529f7443a61202b9bb530beb5d9d394c83ef97
김준겸 <jungyeomkim@gimjungyeoms-MacBook-Pro.local>
2024-04-14 20:17:46 +0900

    JS: test to testlist

--- main.js
+++ main.js
@@ -1,15 +1,24 @@
 const scrollers = document.querySelectorAll(".scroller");
 
 if (!window.matchMedia("(prefers-reduced-motion: reduce)").matches){
-    console.log(scrollers);
+    // console.log(scrollers);
     addAnimation();
-    console.log("no");
+    // console.log("no");
 } else {
-    console.log("yes");
+    // console.log("yes");
 }
 
 function addAnimation(){
     scrollers.forEach((scroller)=>{
         scroller.setAttribute("data-animated", true);
+
+        const scrollerInner = scroller.querySelector(".scroller_inner");
+        const scrollerContent = Array.from(scrollerInner.children);
+
+        scrollerContent.forEach(item => {
+            const duplicatedItem = item.cloneNode(true);
+            duplicatedItem.setAttribute("aria-hidden", true);
+            scrollerInner.appendChild(duplicatedItem);
+        });
     });
 }
