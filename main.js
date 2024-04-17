f7457321129861a15fb7f8362fd726d50c6e178b
김준겸 <jungyeomkim@192.168.nate.com>
2024-04-17 19:45:34 +0900

    add darkMode

--- main.js
+++ main.js
@@ -1,11 +1,7 @@
 const scrollers = document.querySelectorAll(".scroller");
 
 if (!window.matchMedia("(prefers-reduced-motion: reduce)").matches){
-    // console.log(scrollers);
     addAnimation();
-    // console.log("no");
-} else {
-    // console.log("yes");
 }
 
 function addAnimation(){
