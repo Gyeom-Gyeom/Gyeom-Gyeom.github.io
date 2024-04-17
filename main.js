590fb598f411bb4590aebaebe3c4bbe5c1de365e
김준겸 <jungyeomkim@192.168.nate.com>
2024-04-17 22:13:53 +0900

    JS: Dark Mode Goes Up

--- main.js
+++ main.js
@@ -1,9 +1,19 @@
+// Dark Mode
+document.getElementById("dark-mode-changer").addEventListener("click", function() {
+    const body = document.body;
+    if (body.getAttribute("dark-mode") === "dark") {
+        body.setAttribute("dark-mode", "light");
+    } else {
+        body.setAttribute("dark-mode", "dark");
+    }
+});
+
 // Infinite Scroller
 const scrollers = document.querySelectorAll(".scroller");
 
 if (!window.matchMedia("(prefers-reduced-motion: reduce)").matches){
     addAnimation();
-}
+}    
 
 function addAnimation(){
     scrollers.forEach((scroller)=>{
@@ -16,12 +26,7 @@ function addAnimation(){
             const duplicatedItem = item.cloneNode(true);
             duplicatedItem.setAttribute("aria-hidden", true);
             scrollerInner.appendChild(duplicatedItem);
-        });
-    });
-}
-
-// Dark Mode
-const darkModeChanger = document.getElementById("dark-mode-changer");
-
-darkModeChanger.addEventListener("click");
+        });    
+    });    
+}    
 
